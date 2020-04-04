50ad2664cf6f3e349d9e40bc7629d31597014376
KingdomB <kbmajor80@gmail.com>
2020-04-04 13:13:09 -0400

    ...

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.get('/error', (req, res) => {
 
 // Email sent page
 app.get('/email/sent', (req, res) => {
-    res.sendFile('emailMessage.html');
+    res.sendFile('/emailMessage.html');
 });
 
 // Start server
